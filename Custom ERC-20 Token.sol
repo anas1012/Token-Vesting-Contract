b25114dd9b87cd0e51e71f8a8856212a39716395
anas1012 <96229017+anas1012@users.noreply.github.com>
2024-07-25 13:10:52 +0530

    Add files via upload

---
+++ Custom ERC-20 Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import"@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        
+        _mint(msg.sender, 10** decimals());
+        
+        }
+}
